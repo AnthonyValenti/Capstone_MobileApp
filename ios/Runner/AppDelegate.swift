087b07ae88fb63cfbf23ffd3da81ab7bbcf87d97
Anthony Valenti <57304403+AnthonyValenti@users.noreply.github.com>
2023-03-14 19:35:44 -0400

    Added color picker

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -26,6 +26,27 @@ import GoogleMaps
       // This method is invoked on the UI thread.
       turnOff()
     })
+    
+    let controller3 : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel3 = FlutterMethodChannel(name: "capstone.flutter.lights.color",
+                                              binaryMessenger: controller3.binaryMessenger)
+    channel3.setMethodCallHandler({
+     [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        guard let args = call.arguments else {
+            return
+        }
+        if let myArgs = args as? [String: Any],
+           let rParam = myArgs["r"] as? Int,
+           let gParam = myArgs["g"] as? Int,
+           let bParam = myArgs["b"] as? Int{
+            changeColor(r:rParam,g:gParam,b:bParam)
+
+        } else {
+            
+        }
+
+      // This method is invoked on the UI thread.
+    })
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
@@ -93,5 +114,39 @@ private func turnOff(){
     
     task.resume()
 }
+
+
+private func changeColor(r: Int,g: Int,b: Int){
+    guard let url = URL(string: "http://10.0.0.69:8081/zeroconf/dimmable")else{
+        return
+    }
+    var req = URLRequest(url: url)
+    req.httpMethod="POST"
+    req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    let body: [String: Any]=[
+        "deviceid": "10014a2701",
+        "data":[
+ 	        "ltype": "color",
+ 	        "color": ["br": 100, "r": r, "g": g, "b": b]        
+        ]
+    ]
+    req.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+    
+    let task = URLSession.shared.dataTask(with: req){ data, _, error in
+        guard let data = data, error == nil else{
+            return
+        }
+        do{
+            let response = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            print(response)
+        }catch{
+            print(error)
+        }
+        
+    }
+    
+    task.resume()
+}
+
 
 
--- lib/pages/office_page.dart
+++ lib/pages/office_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+const List<String> colorList = <String>['White', 'Blue', 'Red', 'Purple'];
 
 class OfficePage extends StatelessWidget {
   const OfficePage({super.key});
@@ -30,13 +33,20 @@ class SwitchExample extends StatefulWidget {
 }
 
 class _SwitchExampleState extends State<SwitchExample> {
-  bool light1 = true;
-  bool light2 = true;
-  bool light3 = true;
+  bool light1 = false;
+  bool light2 = false;
+  bool light3 = false;
+  String selectedColor = colorList.first;
+  Color selectedC = Colors.white;
+  Map<String, dynamic> selectedColorRBG = {
+    "r": 255,
+    "g": 0,
+    "b": 0,
+  };
 
   static const platform1 = MethodChannel('capstone.flutter.lights.on');
   static const platform2 = MethodChannel('capstone.flutter.lights.off');
-
+  static const platform3 = MethodChannel('capstone.flutter.lights.color');
   Future<void> lightsOn() async {
     try {
       await platform1.invokeMethod('lightsOn');
@@ -48,6 +58,14 @@ class _SwitchExampleState extends State<SwitchExample> {
   Future<void> lightsOff() async {
     try {
       await platform2.invokeMethod('lightsOff');
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> changeColor() async {
+    try {
+      await platform3.invokeMethod('changeColor', selectedColorRBG);
     } on PlatformException catch (e) {
       print(e);
     }
@@ -80,8 +98,56 @@ class _SwitchExampleState extends State<SwitchExample> {
                       setState(() {
                         light1 = value;
                       });
+                      if (light1 == true) {
+                        lightsOn();
+                      }
+                      if (light1 == false) {
+                        lightsOff();
+                      }
                     },
                   ))),
+          DropdownButton<String>(
+            value: selectedColor,
+            icon: const Icon(Icons.palette_outlined),
+            style: const TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String? value) {
+              setState(() {
+                selectedColor = value!;
+                if (selectedColor == "White") {
+                  selectedC = Colors.white;
+                }
+                if (selectedColor == "Blue") {
+                  selectedC = Colors.blue;
+                }
+                if (selectedColor == "Purple") {
+                  selectedC = Colors.purple;
+                }
+                if (selectedColor == "Red") {
+                  selectedC = Colors.red;
+                }
+                selectedColorRBG = {
+                  "r": selectedC.red,
+                  "g": selectedC.green,
+                  "b": selectedC.blue,
+                };
+                changeColor();
+              });
+            },
+            items: colorList.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
           const SizedBox(height: 70),
           const Text(
             "Office Lights:",
@@ -104,36 +170,6 @@ class _SwitchExampleState extends State<SwitchExample> {
                       setState(() {
                         light2 = value;
                       });
-                    },
-                  ))),
-          const SizedBox(height: 70),
-          const Text(
-            "Side Lights:",
-            style: TextStyle(
-              fontSize: 35,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-              width: 100,
-              height: 80,
-              child: FittedBox(
-                  fit: BoxFit.fill,
-                  child: Switch(
-                    // This bool value toggles the switch.
-                    value: light3,
-                    activeColor: Colors.blue,
-                    onChanged: (bool value) {
-                      // This is called when the user toggles the switch.
-                      setState(() {
-                        light3 = value;
-                      });
-                      if (light3 == true) {
-                        lightsOn();
-                      }
-                      if (light3 == false) {
-                        lightsOff();
-                      }
                     },
                   ))),
           const SizedBox(height: 70),
