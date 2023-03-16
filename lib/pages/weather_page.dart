e758213dbe5f6e435ecd55933631b61f9bc8c524
Anthony Valenti <57304403+AnthonyValenti@users.noreply.github.com>
2023-03-16 13:32:02 -0400

    weather UI updated

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:weather/weather.dart';
+import 'package:weather_icons/weather_icons.dart';
 
 WeatherFactory wf = WeatherFactory("618774ba56bdfedf15cce5001b815715");
 
@@ -20,7 +21,15 @@ class WeatherPage extends StatelessWidget {
                 color: Colors.white)),
         backgroundColor: Colors.blueGrey.shade500,
       ),
-      body: const WeatherStates(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/bkg4.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: const WeatherStates(),
+      ),
     );
   }
 }
@@ -37,6 +46,14 @@ class _TempState extends State<WeatherStates> {
   double? _minTemp = 0;
   double? _maxTemp = 0;
   double? _currTemp = 0;
+  double? _rain = 0;
+  double? _wind = 0;
+  String description = " ";
+  TextStyle style2 = const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
+    color: Colors.white,
+  );
 
   @override
   void initState() {
@@ -64,49 +81,155 @@ class _TempState extends State<WeatherStates> {
 
         _currTemp = w.temperature?.celsius;
         _currTemp = roundDouble(_currTemp!, 2);
+
+        description = w.weatherDescription!;
+        _rain = w.rainLast3Hours ?? 0;
+        _wind = w.windSpeed ?? 0;
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style = ElevatedButton.styleFrom(
-      textStyle: const TextStyle(
-        fontSize: 25,
-        fontWeight: FontWeight.bold,
-      ),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-      fixedSize: const Size(350, 100),
-    );
-
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          ElevatedButton(
-            style: style,
-            onPressed: () {},
-            child: Text("Current: $_currTemp °C"),
+          const Icon(
+            WeatherIcons.day_cloudy,
+            color: Colors.white,
+            size: 80,
           ),
-          const SizedBox(height: 60),
-          ElevatedButton(
-            style: style,
-            onPressed: () {},
-            child: Text("Feels Like: $_feelsLike °C"),
+          const Padding(padding: EdgeInsetsDirectional.all(5)),
+          const Text(
+            'Toronto',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w800,
+              color: Colors.white,
+            ),
           ),
-          const SizedBox(height: 60),
-          ElevatedButton(
-            style: style,
-            onPressed: () {},
-            child: Text("Min: $_minTemp °C"),
+          const Padding(padding: EdgeInsetsDirectional.all(30)),
+          const Text(
+            'Currently',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w800,
+              color: Colors.white,
+            ),
           ),
-          const SizedBox(height: 60),
-          ElevatedButton(
-            style: style,
-            onPressed: () {},
-            child: Text("Max: $_maxTemp °C"),
+          const Padding(padding: EdgeInsetsDirectional.all(5)),
+          Text(
+            '$_currTemp °C',
+            style: const TextStyle(
+              fontSize: 55,
+              fontWeight: FontWeight.w900,
+              color: Colors.white,
+            ),
           ),
-          const SizedBox(height: 60),
+          const Padding(padding: EdgeInsetsDirectional.all(5)),
+          Text(
+            description,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w800,
+              color: Colors.white,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(60)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: <Widget>[
+                  Text('$_feelsLike°',
+                      style: const TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      )),
+                  const Text('Feels like',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      )),
+                ],
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: <Widget>[
+                  Text('$_minTemp°',
+                      style: const TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      )),
+                  const Text('Minimum',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      )),
+                ],
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: <Widget>[
+                  Text('$_maxTemp°',
+                      style: const TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      )),
+                  const Text('Minimum',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white,
+                      )),
+                ],
+              ),
+            ],
+          ),
+          const Padding(padding: EdgeInsets.all(40)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                WeatherIcons.cloudy_gusts,
+                color: Colors.white,
+                size: 30,
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              Text(
+                '$_wind m/s',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.all(20)),
+              const Icon(
+                WeatherIcons.raindrop,
+                color: Colors.white,
+                size: 30,
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              Text(
+                '$_rain mm',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
